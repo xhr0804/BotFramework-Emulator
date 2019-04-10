e44cce53b978290a39ee54cb1d37c6700c13d959
Den Scollo <dens.scollo@gmail.com>
2019-04-10 20:13:23 -0300

    Update handlingattachmentbot for test

--- packages/app/client/src/ui/editor/emulator/chatPanel/chatPanel.tsx
+++ packages/app/client/src/ui/editor/emulator/chatPanel/chatPanel.tsx
@@ -30,6 +30,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+
 import * as React from 'react';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { Activity } from '@bfemulator/sdk-shared';
